0031fffa92daa5abb56378191a1a1a3686022d32
colibri-the-bird <120297368+colibri-the-bird@users.noreply.github.com>
2024-05-04 12:14:00 +0500

    nhjnykhiny

--- Assets/Lever.cs
+++ Assets/Lever.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Lever : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject lever1;
     public GameObject lever2;
+
+    public GameObject target;
+    public GameObject R_Platform;
 
     private Vector2 R1;
     private Vector2 R2;
@@ -31,7 +35,15 @@ public class Lever : MonoBehaviour
         if (R2.y > 1) R2.y = 1;
         if (R2.y < -1) R2.y = -1;
 
-        print(R1 + "_" + R2);
+        target.transform.localPosition += new Vector3(0,0,R1.y*Time.deltaTime);
+        float z = target.transform.localPosition.z;
+        float y = target.transform.localPosition.y;
+        if (target.transform.localPosition.z > -2) z = -2;
+        if (target.transform.localPosition.z < -9) z = -9;
+        target.transform.localPosition = new Vector3(0,y,z);
+
+
+        R_Platform.transform.localRotation = Quaternion.Lerp(R_Platform.transform.rotation, Quaternion.Euler(0, R1.x*100, 0), Time.deltaTime);
 
         lever1.transform.localPosition = new Vector3(R1.x, 0, R1.y);
         lever2.transform.localPosition = new Vector3(R2.x, 0, R2.y);
